2b296aec98576a24bb3bdf4d9d717f4404cf6643
NguyenThanhVu028 <23521811@gm.uit.edu.vn>
2026-01-30 23:29:11 +0700

    Implement attendee's movement
    - Make attendee move around within a specific zone
    - Apply sorting axis to determine whether an object is in front or behind other objects

---
+++ Assets/Scripts/Attendee/AttendeeMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class AttendeeMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] float insideSpeed = 5f;
+    [SerializeField] float outsideSpeed = 2f;
+    [SerializeField] TargetZone targetZone;
+    [SerializeField] Vector2 wanderingInterval = new Vector2(1f, 3f);
+    [SerializeField] float minDistanceToTarget = 0.2f;
+
+    private Vector2 wanderingTarget = Vector2.zero;
+    private float wanderingTimer = 0f;
+
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null) Debug.LogWarning("Attendee needs a Rigidbody2D to move");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (targetZone == null || rb == null) return;
+        Wandering();
+    }
+
+    private void Wandering()
+    {
+        wanderingTimer -= Time.fixedDeltaTime;
+        if (wanderingTimer <= 0f)
+        {
+            wanderingTarget = targetZone.GetRelativeRandomPoint();
+            wanderingTimer = Random.Range(wanderingInterval.x, wanderingInterval.y);
+        }
+        Vector2 direction = (wanderingTarget + (Vector2)targetZone.transform.position - (Vector2)transform.position).normalized;
+        if ((wanderingTarget + (Vector2)targetZone.transform.position - (Vector2)transform.position).magnitude < minDistanceToTarget)
+        {
+            Debug.Log("Distance: " + (wanderingTarget - (Vector2)transform.position).magnitude);
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+        rb.linearVelocity = direction * (targetZone.IsPointInsideZone(transform.position) ? insideSpeed : outsideSpeed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
