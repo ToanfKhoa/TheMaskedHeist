bada689339ac7d06ae00f8265c2fade900dca142
kllp031 <31173206+kllp031@users.noreply.github.com>
2026-01-31 02:22:31 +0700

    Update attendee, player prefab, add game manager

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager instance;
+    public static GameManager Instance { 
+        get 
+        {
+            return instance; 
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(instance);
+        }
+        instance = this;
+    }
+
+    public void HandlePlayerFound()
+    {
+        Debug.Log("Player Found! Game Over.");
+        // Additional game over logic here
+    }
+}
