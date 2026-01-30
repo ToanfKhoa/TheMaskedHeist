1bc4988f69c3f16931e4ee4f7b5f5b6c106fd87b
kllp031 <31173206+kllp031@users.noreply.github.com>
2026-01-31 06:51:30 +0700

    Security can now chase you after taking diamond

--- Assets/Scripts/Attendee/Security.cs
+++ Assets/Scripts/Attendee/Security.cs
@@ -2,6 +2,24 @@ using UnityEngine;
 
 public class Security : MonoBehaviour
 {
+    private ChasePlayer2D chasePlayer2D;
+
+    private void Start()
+    {
+        chasePlayer2D = GetComponent<ChasePlayer2D>();
+        chasePlayer2D.enabled = false;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.OnDiamondStolen.AddListener(StartFollow);
+        }
+    }
+
+    void StartFollow()
+    {
+        chasePlayer2D.enabled = true;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
